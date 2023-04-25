9e4fb54b16fdfe39580ca98b358cc65a9f429809
gradientick <camus.luis95@gmail.com>
2023-04-25 14:45:28 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+const API_KEY = "03aee507756406359a342ea6a3a43946";
+let city = "Manila";
+const CURRENT_WEATHER_API =
+  "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}";
